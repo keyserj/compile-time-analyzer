e05ca930f948475699e194e6108b3819d2c36421
Joel Keyser <keyser.joel@gmail.com>
2019-01-06 16:44:40 -0600

    Move solution to root and delete unnecessary extra folder layer

--- CompileTimeAnalyzer/Program.cs
+++ CompileTimeAnalyzer/Program.cs
@@ -2,9 +2,9 @@
 
 namespace CompileTimeAnalyzer
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
